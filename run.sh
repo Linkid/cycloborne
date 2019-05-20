ba87ca8c7c4ef63527c996c3ac5649316132f9e4
François Magimel <magimel.francois@gmail.com>
2019-05-20 21:15:05 +0200

    Fix the question and make the run executable

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ else
     export FLASK_APP=flaskr
     export FLASK_ENV=development
 
-    read -p "Dev version (Y/n)?" -n 1 -r
+    read -p "Init db (Y/n)?" -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
