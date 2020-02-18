0a8e22cf933c406738171878e2376bf3f8da091d
François Magimel <magimel.francois@gmail.com>
2020-02-18 22:32:28 +0100

    Form: use NumberRange instead of DataRequired
    Change the validator for time and dist fields.
    
    Ref #2

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -6,6 +6,7 @@ from wtforms import SubmitField
 from wtforms import IntegerField
 #from wtforms_components import TimeField
 from wtforms.validators import DataRequired
+from wtforms.validators import NumberRange
 
 
 class AskForm(FlaskForm):
@@ -14,7 +15,7 @@ class AskForm(FlaskForm):
         ('vae', _l('VAE')),
     ]
     #cycle_time = TimeField(_l('Time (HH:MM AM)'))
-    cycle_time = IntegerField(_l('<strong>Time (min)</strong>'), validators=[DataRequired()])
-    cycle_dist = FloatField(_l('<strong>Distance (km)</strong>'), validators=[DataRequired()])
+    cycle_time = IntegerField(_l('<strong>Time (min)</strong>'), validators=[NumberRange(min=0, max=200)])
+    cycle_dist = FloatField(_l('<strong>Distance (km)</strong>'), validators=[NumberRange(min=0, max=100)])
     cycle_type = RadioField(_l('<strong>Type of bicycle</strong>'), choices=cycle_type_choices, default='classic', validators=[DataRequired()])
     submit = SubmitField(_l('Submit'))
